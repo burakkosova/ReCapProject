1f460dd0ecceaab1f82cb9d54191a8434229e7a6
Burak Kosova <burak.kosova@hotmail.com>
2021-02-11 23:09:05 +0300

    Added IRentalService and RentalManager for CRUD operations

---
+++ Business/Concrete/RentalManager.cs
@@ -0,0 +1,59 @@
+﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class RentalManager : IRentalService
+    {
+        IRentalDal _rentalDal;
+        public RentalManager(IRentalDal rentalDal)
+        {
+            _rentalDal = rentalDal;
+        }
+        public IResult Add(Rental rental)
+        {
+            _rentalDal.Add(rental);
+            return new SuccesResult(Messages.RentalAdded);
+        }
+
+        public IResult Delete(Rental rental)
+        {
+            if (_rentalDal.Get(r => r.Id == rental.Id) == null)
+            {
+                return new ErrorResult(Messages.RentalNotFound);
+            }
+            _rentalDal.Delete(rental);
+            return new SuccesResult(Messages.RentalDeleted);
+        }
+
+        public IDataResult<List<Rental>> GetAll()
+        {
+            return new SuccessDataResult<List<Rental>>(_rentalDal.GetAll());
+        }
+
+        public IDataResult<Rental> GetById(int rentalId)
+        {
+            if (_rentalDal.Get(r => r.Id == rentalId) == null)
+            {
+                return new ErrorDataResult<Rental>(Messages.RentalNotFound);
+            }
+            return new SuccessDataResult<Rental>(_rentalDal.Get(r => r.Id == rentalId));
+        }
+
+        public IResult Update(Rental rental)
+        {
+            if (_rentalDal.Get(r => r.Id == rental.Id) == null)
+            {
+                return new ErrorResult(Messages.RentalNotFound);
+            }
+            _rentalDal.Update(rental);
+            return new ErrorResult(Messages.RentalUpdated);
+        }
+    }
+}
--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -7,15 +7,18 @@ namespace Business.Constants
     public static class Messages
     {
         public static string CarAdded = "Araç eklendi";
+        public static string RentalAdded = "Kiralama bilgisi eklendi";
         public static string UserAdded = "Kullanıcı eklendi";
         public static string CarDeleted = "Araç silindi.";
         public static string UserDeleted = "Kullanıcı silindi.";
         public static string CarUpdated= "Araç güncellendi.";
         public static string UserUpdated= "Kullanıcı güncellendi.";
+        public static string RentalUpdated= "Kiralama bilgisi güncellendi.";
         public static string CustomerUpdated= "Müşteri güncellendi.";
         public static string BrandAdded = "Marka eklendi.";
         public static string BrandDeleted = "Marka silindi.";
         public static string CustomerDeleted = "Müşteri silindi.";
+        public static string RentalDeleted = "Kiralama bilgisi silindi.";
         public static string BrandUpdated = "Marka güncellendi";
         public static string ColorAdded = "Renk eklendi.";
         public static string ColorDeleted = "Renk silindi.";
@@ -27,6 +30,7 @@ namespace Business.Constants
         public static string ColorNotFound = "Renk bulunamadı.";
         public static string CustomerNotFound = "Müşteri bulunamadı.";
         public static string UserNotFound = "Renk bulunamadı.";
+        public static string RentalNotFound = "Kiralama bilgisi bulunamadı.";
         public static string BrandAlreadyExists = "Marka veritabanında zaten var!";
         public static string ColorAlreadyExists = "Renk veritabanında zaten var!";
         public static string CustomerAdded = "Müşteri eklendi";
