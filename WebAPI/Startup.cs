1837f25991d1b2b7095e40f21a19f2a4a6644540
Burak Kosova <burak.kosova@bahcesehir.edu.tr>
2021-02-19 06:47:17 +0300

    Added Rentals Controller

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -42,6 +42,8 @@ namespace WebAPI
             services.AddSingleton<IUserDal, EfUserDal>();
             services.AddSingleton<ICustomerService, CustomerManager>();
             services.AddSingleton<ICustomerDal, EfCustomerDal>();
+            services.AddSingleton<IRentalService, RentalManager>();
+            services.AddSingleton<IRentalDal, EfRentalDal>();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPI", Version = "v1" });
